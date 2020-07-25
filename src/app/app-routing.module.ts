e2225bacfdb71db4308e451183de792fe28bd2a1
Zisan Karsatar <zisankarsatar@gmail.com>
2020-07-25 23:31:44 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { noUndefined } from '@angular/compiler/src/util';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  {
+    path:"",component:HomeComponent
+  },
+  {
+    path:"about", component:AboutComponent
+  },
+  {
+    path:"**", component:NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
