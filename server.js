a09b3fdc151827657ead6c26587325da19f92af1
Zisan Karsatar <zisankarsatar@gmail.com>
2020-08-03 23:49:28 +0300

    srver.jsv5 changed

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const path= require("path");
 
-app.use(express.static(__dirname+ "./dist"));
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname+ "/TodosApp-Angular7"));
 
 app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname+"./dist/TodosApp-Angular7/index.html"));
+    res.sendFile(path.join(__dirname+"/TodosApp-Angular7/index.html"));
     
 });
 
+app.listen(process.env.PORT || 8080);
 console.log("Console listening!");
